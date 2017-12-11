d1431dbd047dba1d5635cde4579944fb58d58c9d
Jeremy Worboys <jw@jeremyworboys.com>
2017-12-12 09:46:41 +1100

    Add link finder

---
+++ src/Download/LinkFinder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace JeremyWorboys\SonarrPutIO\Download;
+
+use PutIO\Helpers\PutIO\PutIOHelper;
+
+class LinkFinder extends PutIOHelper
+{
+    /**
+     * @param int  $fileID
+     * @param bool $mediaOnly
+     * @return array
+     */
+    public function getDownloadLinks($fileID, $mediaOnly = \false)
+    {
+        $params = [
+            'file_ids' => $fileID,
+        ];
+
+        $key = $mediaOnly ? 'media_links' : '';
+
+        return $this->get('files/get-download-links', $params, \false, $key);
+    }
+}
