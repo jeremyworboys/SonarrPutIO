350d8c4d583fccec88158ee5224363e384dff482
Jeremy Worboys <jw@jeremyworboys.com>
2017-12-14 20:17:06 +1100

    Add config to container

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,13 @@ use League\Container\Container;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$config = require __DIR__ . '/config.php';
+
 $app = new Container();
+$app->add('config', $config);
+foreach ($config as $key => $value) {
+    $app->add('config.' . $key, $value);
+}
 
 $app->share('macpsd', function () {
     return new ProgressiveDownloader();
---
+++ bootstrap/config.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'putio_token'     => '***REMOVED***',
+    'media_directory' => '/Users/jeremyworboys/Downloads/Media',
+];
