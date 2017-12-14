ddf2405dd93af4dd21ddc9fcab32639e1b49310a
Jeremy Worboys <jw@jeremyworboys.com>
2017-12-14 20:42:12 +1100

    Fix container usage

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,10 +3,10 @@
 use JeremyWorboys\SonarrPutIO\DownloadHandler;
 use JeremyWorboys\SonarrPutIO\Infrastructure\FlatFile\FlatFileDownloadRepository;
 use JeremyWorboys\SonarrPutIO\Infrastructure\FlatFile\FlatFileTransferRepository;
+use JeremyWorboys\SonarrPutIO\Service\ProgressiveDownloader;
 use JeremyWorboys\SonarrPutIO\SonarrDownloadHandler;
 use JeremyWorboys\SonarrPutIO\SonarrGrabHandler;
 use JeremyWorboys\SonarrPutIO\SonarrHandler;
-use JeremyWorboys\SonarrPutIO\Service\ProgressiveDownloader;
 use League\Container\Container;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -23,45 +23,45 @@ $app->share('macpsd', function () {
     return new ProgressiveDownloader();
 });
 
-$app->share('putio', function (Container $app) {
+$app->share('putio', function () use ($app) {
     $token = $app->get('config.putio_token');
     $putio = new PutIO\API($token);
     $putio->setSSLVerifyPeer(false);
     return $putio;
 });
 
-$app->share('download_repository', function (Container $app) {
+$app->share('download_repository', function () use ($app) {
     $database = $app->get('config.database_directory') . '/downloads.txt';
     return new FlatFileDownloadRepository($database);
 });
 
-$app->share('transfer_repository', function (Container $app) {
+$app->share('transfer_repository', function () use ($app) {
     $database = $app->get('config.database_directory') . '/transfers.txt';
     return new FlatFileTransferRepository($database);
 });
 
-$app->share('sonarr_handler', function (Container $app) {
+$app->share('sonarr_handler', function () use ($app) {
     return new SonarrHandler(
         $app->get('sonarr_handler.grab'),
         $app->get('sonarr_handler.download')
     );
 });
 
-$app->share('sonarr_handler.grab', function (Container $app) {
+$app->share('sonarr_handler.grab', function () use ($app) {
     return new SonarrGrabHandler(
         $app->get('putio'),
         $app->get('transfer_repository')
     );
 });
 
-$app->share('sonarr_handler.download', function (Container $app) {
+$app->share('sonarr_handler.download', function () use ($app) {
     return new SonarrDownloadHandler(
         $app->get('putio'),
         $app->get('download_repository')
     );
 });
 
-$app->share('download_handler', function (Container $app) {
+$app->share('download_handler', function () use ($app) {
     return new DownloadHandler(
         $app->get('putio'),
         $app->get('macpsd'),
